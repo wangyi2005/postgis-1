ccb693bbbdebde6533841af6d3632ee7df2c4b5b
wangyi2005 <wangyi2005@users.noreply.github.com>
2018-04-25 21:05:17 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -31,16 +31,19 @@ fi
 if [ "$1" = 'postgres' ] && [ "$(id -u)" = '0' ]; then
 	mkdir -p "$PGDATA"
 	chown -R postgres "$PGDATA"
+	chgrp -R 0 "$PGDATA"
 	chmod 700 "$PGDATA"
 
 	mkdir -p /var/run/postgresql
 	chown -R postgres /var/run/postgresql
+	chgrp -R 0 /var/run/postgresql
 	chmod 775 /var/run/postgresql
 
 	# Create the transaction log directory before initdb is run (below) so the directory is owned by the correct user
 	if [ "$POSTGRES_INITDB_XLOGDIR" ]; then
 		mkdir -p "$POSTGRES_INITDB_XLOGDIR"
 		chown -R postgres "$POSTGRES_INITDB_XLOGDIR"
+		chgrp -R 0 "$POSTGRES_INITDB_XLOGDIR"
 		chmod 700 "$POSTGRES_INITDB_XLOGDIR"
 	fi
 
@@ -50,6 +53,7 @@ fi
 if [ "$1" = 'postgres' ]; then
 	mkdir -p "$PGDATA"
 	chown -R "$(id -u)" "$PGDATA" 2>/dev/null || :
+	chgrp -R 0 "$PGDATA" 2>/dev/null || :
 	chmod 700 "$PGDATA" 2>/dev/null || :
 
 	# look specifically for PG_VERSION, as it is expected in the DB dir
@@ -142,6 +146,3 @@ if [ "$1" = 'postgres' ]; then
 fi
 
 exec "$@"
-
-cd $GEOSERVER_HOME/bin
-./startup.sh
